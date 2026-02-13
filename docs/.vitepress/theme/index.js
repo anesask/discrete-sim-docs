6f1057b9cfa726cafeff4442ef9154e33801f7ca
Anes Mulalic <anes.mulalic@hotmail.com>
2026-02-13 22:44:51 +0100

    Removed interactive examples and vue-echarts reference

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,18 +1,10 @@
 import DefaultTheme from 'vitepress/theme'
 import './custom.css'
-import SimulationPlayground from './components/SimulationPlayground.vue'
-import MM1QueuePlayground from './components/MM1QueuePlayground.vue'
-import CoffeeShopPlayground from './components/CoffeeShopPlayground.vue'
 
 export default {
   extends: DefaultTheme,
   enhanceApp({ app }) {
     // Inject current year globally
     app.config.globalProperties.$currentYear = new Date().getFullYear()
-
-    // Register custom components
-    app.component('SimulationPlayground', SimulationPlayground)
-    app.component('MM1QueuePlayground', MM1QueuePlayground)
-    app.component('CoffeeShopPlayground', CoffeeShopPlayground)
   }
 }
